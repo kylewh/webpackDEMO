1806c80d41f986beb0a0c3282487a58d8116ee6a
kylewh <kylevanho@gmail.com>
2017-04-01 15:15:10 +0800

    Finished

---
+++ webpack.config.js
@@ -0,0 +1,15 @@
+var path = require('path')
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [
+    new webpack.ProvidePlugin({
+      $: jquery,
+      jQuery: jquery
+    })
+  ]
+}
